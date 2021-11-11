3400e9afceae5a097102abce5d2227274f9c9612
lixiangdao <xiangdao.li@iquantex.com>
2021-11-11 09:46:52 +0800

    init project

---
+++ codecoffe/src/main/java/com/leexdao/codeexcise/CodeexciseApplication.java
@@ -0,0 +1,13 @@
+package com.leexdao.codeexcise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeexciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeexciseApplication.class, args);
+    }
+
+}
---
+++ codecoffe/src/test/java/com/leexdao/codeexcise/CodeexciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leexdao.codeexcise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeexciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
